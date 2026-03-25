02cec83f086c5cad04efdb6edeae6e1849a5079e
Dalton Kajander <daltonkajander@yahoo.com>
2026-03-25 08:23:52 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -1,14 +1,12 @@
 from dataclasses import dataclass, field
 from functools import total_ordering
-from abc import ABC
+from abc import ABC, abstractmethod
 from random import shuffle as rand_shuffle, seed as rand_seed
 
-@dataclass
-class Card(ABC): pass
 
 @total_ordering
 @dataclass()
-class PlayingCard(Card):
+class PlayingCard():
     rank: str
     suit: str
     
@@ -77,11 +75,11 @@ class PlayingCard(Card):
     
 @dataclass
 class Deck:
-    cards: list = field(default_factory = list[Card])
+    cards: list = field(default_factory = list[PlayingCard])
     
     def generate_playingcard_deck(self) -> None:
-        for rank in PlayingCard._ranks:
-            for suit in PlayingCard._suits:
+        for suit in PlayingCard._suits:
+            for rank in PlayingCard._ranks:
                 self.cards.append(PlayingCard(rank, suit))
                 
     def shuffle(self, seed: int | None = None) -> None:
@@ -92,28 +90,137 @@ class Deck:
         for i in range(count):
             hand.append(self.pop())
             
-    def append(self, card: Card) -> None: self.cards.append(card)
+    def append(self, card: PlayingCard) -> None: self.cards.append(card)
     
     def deal_to_hands(self, hands: list[Hand], cards_per_hand: int = 1) -> None:
         for i in range(cards_per_hand):
             for hand in hands:
                 hand.append(self.pop())
                 
-    def pop(self) -> Card:
-        return self.cards.pop()
-    
+    def pop(self) -> PlayingCard: return self.cards.pop()
     
 @dataclass
 class Hand:
-    cards: list = field(default_factory = list[Card])
+    cards: list[PlayingCard] = field(default_factory = list)
     
-    def append(self, card: Card) -> None: self.cards.append(card)
+    def append(self, card: PlayingCard) -> None: self.cards.append(card)
     
+    def draw(self, deck: Deck, count: int = 1) -> None:
+        for i in range(count):
+            self.append(deck.pop())
+            
+    def __len__(self) -> int: return len(self.cards)
+    
+    def sort(self, reverse = False) -> None: self.cards.sort(reverse = not reverse)
+    
+    def __iter__(self):
+        self._iter_index = 0
+        return self
+    
+    def __next__(self) -> PlayingCard:
+        if self._iter_index >= len(self.cards): raise StopIteration
+        card = self.cards[self._iter_index]
+        self._iter_index += 1
+        return card
+
+    def has_straight(self) -> bool:
+        if (len(self) < 5): return False
+        hand_copy = Hand(self.cards[:])
+        hand_copy.sort()
+        curr_cards = []
+        for index,card in enumerate(hand_copy):
+            if (len(curr_cards) == 0): 
+                curr_cards.append(card)
+                continue
+            
+            last_rank = get_rank_value(curr_cards[-1].rank)
+            curr_rank = get_rank_value(card.rank)
+            
+            if (last_rank == curr_rank + 1): curr_cards.append(card)
+            elif (last_rank == curr_rank): continue
+            else: curr_cards = [curr_cards[-1]] # We found a card that is too far from the last card, resetting at this new point
+            
+            if (len(curr_cards) >= 5): return True
+
+        
+        return False
+    
+    def evaluate(self) -> int:
+        hand_copy = Hand(self.cards[:])
+        hand_copy.sort()
+        
+        rank_counts = {}
+        suit_counts = {}
+        for card in hand:
+            if (card.rank not in rank_counts): rank_counts[card.rank] = [card]
+            else: rank_counts[card.rank].append(card)
+
+            if (card.suit not in suit_counts): suit_counts[card.suit] = [card]
+            else: suit_counts[card.suit].append(card)
+            
+        for rank in rank_counts: rank_counts[rank].sort(reverse = True)
+        for suit in suit_counts: suit_counts[suit].sort(reverse = True)
+        
+        hands_satisfied = [0]
+        
+        # 10. Royal flush - Ace high straight and flush
+        if (len(hand) >= 5): # Hand requires at least 5 cards
+            for suit in suit_counts:
+                if (len(suit_counts[suit]) != 5): continue # Current suit does not have enough cards
+                for card in suit_counts[suit]:
+                    print(card, end = " ")
+                print()
+        
+        # 9. Straight flush - Straight and flush
+        if (len(hand) >= 5): # Hand requires at least 5 cards
+            ...
+            
+        # 8. Four of a kind - Four of the same rank
+        if (len(hand) >= 4): # Hand requires at least 4 cards
+            ...
+            
+        # 7. Full house - Two of one rank and 3 of another rank
+        if (len(hand) >= 5): # Hand requires at least 5 cards
+            ...
+            
+        # 6. Flush - Five of the same suit
+        if (len(hand) >= 5): # Hand requires at least 5 cards
+            ...
+            
+        # 5. Straight - Five consecutive ranks (Ace counts as 14 and 1)
+        if (len(hand) >= 5): # Hand requires at least 5 cards
+            ...
+            
+        # 4. Three of a kind - Three of the same rank
+        if (len(hand) >= 3): # Hand requires at least 3 cards
+            ...
+
+        # 3. Two pair - Two of one rank and two of another rank
+        if (len(hand) >= 4): # Hand requires at least 4 cards
+            ...
+
+        # 2. Pair - Two of the same rank
+        if (len(hand) >= 2): # Hand requires at least 2 cards
+            ...
+        
+        if (len(hand) != 0): hands_satisfied.append(1) # 1. High card - Any single card
+        return max(hands_satisfied)
+    
+
+def get_rank_value(rank: str) -> int:
+    try:
+        value = int(rank)
+    except:
+        value = ['j', 'q', 'k', 'a'].index(rank) + 11
+    return value
+
+
+
 deck = Deck()
 deck.generate_playingcard_deck()
-deck.shuffle()
 
-hand_1 = Hand()
-hand_2 = Hand()
-deck.deal_to_hands([hand_1, hand_2], 5)
-print(hand_1, hand_2)
+hand = Hand()
+hand.draw(deck, 5)
+rand_shuffle(hand.cards)
+print(f"Has straight: {hand.has_straight()}")
+print(f"Hand score: {hand.evaluate()}")
