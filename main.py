1ed52ec2651da814f29b046286d7d3f74f99d5a9
Dalton Kajander <daltonkajander@yahoo.com>
2026-05-15 10:10:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,4 @@ import gambling.dice as dice
 
 
 if __name__ == "__main__":
-    n = 100000
-    sides = [2, 4, 6, 8, 10, 12, 20, 100]
-    rolls = 50
-    avgs = []
-    for side_count in sides:
-        print(f"Rolling {rolls}d{side_count}")
-        adv_rolls = [sum(dice.roll_dice(f"{rolls}d{side_count}kh1")) for _ in range(n)]
-        
-        d = dice.Die(side_count)
-        normal_rolls = [d.roll() for _ in range(n)]
-        avgs.append((sum(adv_rolls) / len(adv_rolls), sum(normal_rolls) / len(normal_rolls)))
-
-
-    for index in range(len(sides)):
-        print(f"Sides: {sides[index]}")
-        print(f"\tAverage roll: {avgs[index][1]}")
-        print(f"\tAverage advantage roll: {avgs[index][0]}")
+    ...
